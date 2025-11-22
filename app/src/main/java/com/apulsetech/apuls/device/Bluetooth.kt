849dcd2af9986be4c28ca3eb609b6cd91b5ebaa5
Sharp0802 <sharp0802.business@gmail.com>
2025-11-22 19:22:20 +0900

    :bulb: Report bluetooth connection failure

--- app/src/main/java/com/apulsetech/apuls/device/Bluetooth.kt
+++ app/src/main/java/com/apulsetech/apuls/device/Bluetooth.kt
@@ -7,6 +7,7 @@ import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
 import android.content.Context
 import android.content.pm.PackageManager
+import android.util.Log
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
@@ -33,6 +34,7 @@ internal class BluetoothDevice : Device {
             socket.connect()
             BluetoothDeviceSocket(socket)
         } catch (e: IOException) {
+            Log.e("BluetoothDevice", e.toString())
             try {
                 socket.close()
             } catch (_: IOException) {
