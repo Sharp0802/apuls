ec3d68cb614cbf689123df68ee111b31ba60d38c
Sharp0802 <sharp0802.business@gmail.com>
2025-11-24 04:33:45 +0900

    :recycle: Adjust padding between setting groups

--- app/src/main/java/com/apulsetech/apuls/views/Settings.kt
+++ app/src/main/java/com/apulsetech/apuls/views/Settings.kt
@@ -1,8 +1,11 @@
 package com.apulsetech.apuls.views
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.apulsetech.apuls.command.CommandDeclaration
 import com.apulsetech.apuls.command.CommandDeclarations
 import com.apulsetech.apuls.command.CommandDeclarations.*
@@ -20,6 +23,8 @@ fun Settings(comm: DeviceCommViewModel, modifier: Modifier = Modifier) {
     }
 
     Column(modifier) {
+        val padding = 16.dp
+
         when (comm.connectionType) {
             // TODO : add tcp way to connect
 
@@ -27,6 +32,8 @@ fun Settings(comm: DeviceCommViewModel, modifier: Modifier = Modifier) {
                 Group("Serial", baudrate)
             }
         }
+
+        Spacer(Modifier.height(padding))
 
         Group(
             "Reader",
@@ -36,16 +43,29 @@ fun Settings(comm: DeviceCommViewModel, modifier: Modifier = Modifier) {
             alivetime,
         )
 
+        Spacer(Modifier.height(padding))
+
         Group("Antenna General", antseq, idle)
 
+        Spacer(Modifier.height(padding))
+
         Group("Antenna 1", ant1, power1, dwell1, filt_rssi1)
+        Spacer(Modifier.height(padding))
         Group("Antenna 2", ant2, power2, dwell2, filt_rssi2)
+        Spacer(Modifier.height(padding))
         Group("Antenna 3", ant3, power3, dwell3, filt_rssi3)
+        Spacer(Modifier.height(padding))
         Group("Antenna 4", ant4, power4, dwell4, filt_rssi4)
+
+        Spacer(Modifier.height(padding))
 
         Group("Q Algorithm", startq, minq, maxq)
 
+        Spacer(Modifier.height(padding))
+
         Group("Select Filter", select1, select2, select3, select4)
+
+        Spacer(Modifier.height(padding))
 
         Group(
             "Report",
@@ -61,13 +81,23 @@ fun Settings(comm: DeviceCommViewModel, modifier: Modifier = Modifier) {
             tagtimeout
         )
 
+        Spacer(Modifier.height(padding))
+
         Group("Misc", buzz)
+
+        Spacer(Modifier.height(padding))
 
         Group("System", version, fwtype, rid, model, region, date, serialno)
 
+        Spacer(Modifier.height(padding))
+
         Group("Serve", ser_serial, ser_tcp, ser_http, ser_mqtt)
 
+        Spacer(Modifier.height(padding))
+
         Group("Http", httpurl, httpauth, httpuser, httppwd)
+
+        Spacer(Modifier.height(padding))
 
         Group("Mqtt", mqttbroker, mqtttopic, mqttqos, mqttalive)
     }
