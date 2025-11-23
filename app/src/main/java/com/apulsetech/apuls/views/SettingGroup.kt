e74bacceddbdd3245fa4dba894d023ae3d29c446
Sharp0802 <sharp0802.business@gmail.com>
2025-11-24 04:33:55 +0900

    :recycle: Make group title bold

--- app/src/main/java/com/apulsetech/apuls/views/SettingGroup.kt
+++ app/src/main/java/com/apulsetech/apuls/views/SettingGroup.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.apulsetech.apuls.command.ParameterizedCommandDeclaration
 
@@ -32,6 +33,7 @@ fun SettingGroup(
         Column {
             Text(
                 text = name,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(8.dp)
             )
 
