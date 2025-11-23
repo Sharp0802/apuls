ee209f7ec3ea17bfc9ece6f11a9a3f6e4a09fa2d
Sharp0802 <sharp0802.business@gmail.com>
2025-11-24 02:33:52 +0900

    :sparkles: Implement string renderer

---
+++ app/src/main/java/com/apulsetech/apuls/data/compose/StringRenderer.kt
@@ -0,0 +1,73 @@
+package com.apulsetech.apuls.data.compose
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.KeyboardType
+import com.apulsetech.apuls.command.IConstraint
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class StringRenderer : IRenderer<String> {
+    data class State(
+        val value: String? = null,
+        val error: String? = null
+    )
+
+    private val _state = MutableStateFlow(State())
+    private val state = _state.asStateFlow()
+
+    @Composable
+    override fun Render(
+        value: String,
+        constraints: Array<IConstraint>,
+        onValueChanged: (String) -> Unit,
+        enabled: Boolean,
+        modifier: Modifier
+    ) {
+        val state by state.collectAsState()
+
+        val isError = state.error != null
+
+        if (state.value == null) {
+            _state.value = _state.value.copy(value = value)
+        }
+
+        TextField(
+            value = state.value ?: "",
+            onValueChange = {
+                _state.value = _state.value.copy(value = it)
+
+                var noError = true
+                for (constraint in constraints) {
+                    if (constraint.validate(it))
+                        continue
+
+                    _state.value = _state.value.copy(error = constraint.toString())
+                    noError = false
+                }
+                if (noError) {
+                    _state.value = _state.value.copy(error = null)
+                    onValueChanged(it)
+                }
+            },
+            isError = isError,
+            supportingText = {
+                if (isError) {
+                    Text(
+                        text = state.error ?: "",
+                        modifier = Modifier.semantics {
+                            error(state.error ?: "")
+                        }
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+        )
+    }
+}
